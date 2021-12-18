4dfbebb721ed9a1e7eb17a8287cbafb4db78093b
Willem Verbuyst <willemverbuyst@gmail.com>
2021-12-18 19:38:46 +0100

    Add app bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Books from './pages/books';
 import Root from './pages/root';
 import PageNotFound from './pages/pageNotFound';
+import AppBar from './components/AppBar';
 
 const App = () => {
   return (
     <div className="App">
+      <AppBar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Root />} />
