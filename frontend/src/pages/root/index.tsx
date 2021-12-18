ad0c1d3e4b5bfaa3d1b4d17929146852021a07ad
Willem Verbuyst <willemverbuyst@gmail.com>
2021-12-18 17:54:38 +0100

    Add overmind dev tools

--- frontend/src/pages/root/index.tsx
+++ frontend/src/pages/root/index.tsx
@@ -1,7 +1,12 @@
 import { ReactElement } from 'react';
 
 const Root: React.FC = (): ReactElement => {
-  return <h1>Root</h1>;
+  return (
+    <>
+      <h1>Root</h1>
+      <button>LOGIN</button>
+    </>
+  );
 };
 
 export default Root;
