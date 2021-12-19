9a476f16e396cc15397cffaad32304c145d3b092
Willem Verbuyst <willemverbuyst@gmail.com>
2021-12-19 12:34:01 +0100

    Work on state

--- frontend/src/overmind/actions.ts
+++ frontend/src/overmind/actions.ts
@@ -1,14 +1,17 @@
 import { Context } from '.';
 
-export const loginUser = ({ state }: Context) => {
+export const loginUser = async ({ effects, state }: Context) => {
   state.isLoggedIn = true;
+  const allBooks = await effects.api.getAllBooks();
+  state.books = allBooks;
 };
 
 export const logoutUser = ({ state }: Context) => {
   state.isLoggedIn = false;
+  state.books = [];
 };
 
-export const fetchAllBooks = async ({ effects, state }: Context) => {
-  const allBooks = await effects.api.getAllBooks();
-  state.books = allBooks;
-};
+// export const fetchAllBooks = async ({ effects, state }: Context) => {
+//   const allBooks = await effects.api.getAllBooks();
+//   state.books = allBooks;
+// };
--- frontend/src/pages/root/index.tsx
+++ frontend/src/pages/root/index.tsx
@@ -1,10 +1,8 @@
 import { Button } from '@mui/material';
 import { ReactElement } from 'react';
-import { useActions } from '../../overmind';
 import { useNavigate } from 'react-router-dom';
 
 const Root: React.FC = (): ReactElement => {
-  const { fetchAllBooks } = useActions();
   const navigate = useNavigate();
 
   const gotoLogin = () => navigate('/login');
@@ -13,7 +11,6 @@ const Root: React.FC = (): ReactElement => {
     <>
       <h1>Home</h1>
       <Button onClick={gotoLogin}>Go to login page</Button>
-      <button onClick={() => fetchAllBooks()}>books</button>
     </>
   );
 };
