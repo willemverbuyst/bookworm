d43f769e57896be9e5bf08b3d67a0860b539d08c
Willem Verbuyst <willemverbuyst@gmail.com>
2021-12-20 05:47:19 +0100

    Add comments, clean up

--- backend/database/csv_to_sql.sh
+++ backend/database/csv_to_sql.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
-# https://github.com/pavanchhatpar/csv-to-sql-converter
 
+# Based on: https://github.com/pavanchhatpar/csv-to-sql-converter
+
+# Get file, in same directory
 file_name="$(dirname -- "$0")/books_copy.csv"
 
+# Create temp file
 cat $file_name > tmp.csv
+
+# New file name
 new_file="bookworm.sql"
+
+# Name of table in backticks
 table="\`book\`"
 
+# COLUMN NAMES
+# Replace pipe with backtick comma backtick 
+# Delete quotes and newline
 columns=$(head --lines=1 tmp.csv | sed 's/|/`,`/g' | tr -d "\"\r\n")
+
+# Add first and last backtick
 columns="\`$columns\`"
 
+# VALUES
+# Loop through lines in temp csv, last line needs terminating linefeed!
+# Replace pipe with comma
+# Delete newline
 tail --lines=+2 tmp.csv | while read l ; do
 values=$(echo $l | sed 's/|/,/g' | tr -d "\r\n")
-values="$values"
 
-echo "INSERT INTO $op($columns) VALUES ($values);"
-done > "$new_file"
+echo "INSERT INTO $table($columns) VALUES ($values);"
+done > $new_file
 
+# Remove temp file
 rm tmp.csv
