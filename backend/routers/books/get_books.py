70ee8b8706be29e547e84da2db8655a6fc96dd0e
Willem Verbuyst <willemverbuyst@gmail.com>
2021-12-24 05:44:17 +0100

    Add helper functions

--- backend/routers/books/get_books.py
+++ backend/routers/books/get_books.py
@@ -7,43 +7,11 @@ from database.connect import get_books_from_db
 get_books_router = APIRouter()
 
 
-def get_books():
-    books = []
-    with open("dummy_data/books.csv") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter="|")
-
-        for row in csv_reader:
-            book = {
-                "id": uuid.uuid4(),
-                "title": row[0],
-                "language": row[1],
-                "author": row[2],
-                "year": int(row[3]),
-                "read": int(row[4]),
-            }
-            books.append(book)
-
-    return books
-
-
 @get_books_router.get("/books")
 def get_all_books():
     try:
-        all_books = get_books_from_db()
-        result = len(all_books)
-
-        books = []
-        for book in all_books:
-            book = {
-                "id": book[0],
-                "title": book[1],
-                "language": book[2],
-                "author": book[3],
-                "year": book[4],
-                "read": book[5],
-            }
-            books.append(book)
-
+        books = get_books_from_db()
+        result = len(books)
 
         return {
             "status": "succes",
