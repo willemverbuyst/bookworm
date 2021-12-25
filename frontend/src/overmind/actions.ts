2416f95ad96be5aac51505c684c3bf498aaec1f4
Willem Verbuyst <willemverbuyst@gmail.com>
2021-12-25 07:40:33 +0100

    Store fake token

--- frontend/src/overmind/actions.ts
+++ frontend/src/overmind/actions.ts
@@ -2,16 +2,21 @@ import { Context } from '.';
 
 export const loginUser = async ({ effects, state }: Context) => {
   state.isLoggedIn = true;
+  localStorage.setItem('token', 'access_token');
   const allBooks = await effects.api.getAllBooks();
   state.booksApi = allBooks;
 };
 
 export const logoutUser = ({ state }: Context) => {
   state.isLoggedIn = false;
+  localStorage.removeItem('token');
+
   state.booksApi = [];
 };
 
-// export const fetchAllBooks = async ({ effects, state }: Context) => {
-//   const allBooks = await effects.api.getAllBooks();
-//   state.books = allBooks;
-// };
+export const onInitializeOvermind = ({ state }: Context) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    state.isLoggedIn = true;
+  }
+};
