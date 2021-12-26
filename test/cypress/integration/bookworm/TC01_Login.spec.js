3ae0e55ae3264c6716bf72b1eb967efb785a2019
Willem Verbuyst <willemverbuyst@gmail.com>
2021-12-26 09:34:20 +0100

    Use custom commands

--- test/cypress/integration/bookworm/TC01_Login.spec.js
+++ test/cypress/integration/bookworm/TC01_Login.spec.js
@@ -6,26 +6,14 @@ describe('Login', function () {
   });
 
   it('Login - Valid Credentials', function () {
-    cy.visit('/');
-    cy.get('.title').should('contain', 'Home');
-    cy.contains('go to login page').click();
-    cy.get('.title').should('contain', 'Login');
-    cy.get('input[type="email"]').type(this.data.email);
-    cy.get('input[type="password"]').type(this.data.password);
-    cy.get('button[type="submit"]').click();
+    cy.bookwormLogin(this.data.email, this.data.password);
     cy.get('.title').should('contain', 'Books');
     cy.contains('log out').click();
     cy.get('.title').should('contain', 'Home');
   });
 
   it('Login - Invalid Credentials', function () {
-    cy.visit('/');
-    cy.get('.title').should('contain', 'Home');
-    cy.contains('go to login page').click();
-    cy.get('.title').should('contain', 'Login');
-    cy.get('input[type="email"]').type(this.data.email);
-    cy.get('input[type="password"]').type(this.data.wrongPassword);
-    cy.get('button[type="submit"]').click();
+    cy.bookwormLogin(this.data.email, this.data.wrongPassword);
     cy.contains('User with this email and password not found!');
   });
 });
