bb76126e683d43a78256809a4e016ea16ffd737b
Willem Verbuyst <willemverbuyst@gmail.com>
2021-12-28 20:08:10 +0100

    Adjust seed file

--- backend/database/python/seed_db.py
+++ backend/database/python/seed_db.py
@@ -4,8 +4,10 @@ from helpers.sql_helpers import executeScriptsFromFile
 
 dirname = os.path.dirname(__file__)
 sql_helpers = os.path.join(dirname, "../helpers/sql_helpers.py")
+create_author_table_sql = os.path.join(dirname, "../sql/book/create_author_table.sql")
 create_book_table_sql = os.path.join(dirname, "../sql/book/create_book_table.sql")
 create_user_table_sql = os.path.join(dirname, "../sql/user/create_user_table.sql")
+insert_authors_sql = os.path.join(dirname, "../sql/book/insert_authors.sql")
 insert_books_sql = os.path.join(dirname, "../sql/book/insert_books.sql")
 insert_users_sql = os.path.join(dirname, "../sql/user/insert_users.sql")
 
@@ -21,8 +23,10 @@ def seed_db():
 
     cursor = conn.cursor()
 
+    executeScriptsFromFile(create_author_table_sql, cursor)
     executeScriptsFromFile(create_book_table_sql, cursor)
     executeScriptsFromFile(create_user_table_sql, cursor)
+    executeScriptsFromFile(insert_authors_sql, cursor)
     executeScriptsFromFile(insert_books_sql, cursor)
     executeScriptsFromFile(insert_users_sql, cursor)
 
