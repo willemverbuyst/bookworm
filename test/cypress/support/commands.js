ca5674f2fa0f7f2d15a35dff0d72e6c66e7a28eb
Willem Verbuyst <willemverbuyst@gmail.com>
2021-12-28 13:06:05 +0100

    Clean up

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -2,13 +2,14 @@ import HomePage from '../integration/pages/homePage'
 import LoginPage from '../integration/pages/loginPage'
 
 Cypress.Commands.add('bookwormLogin', function (email, password) {
-	const home = new HomePage()
+	const homePage = new HomePage()
 	const loginPage = new LoginPage()
 
 	cy.visit('/')
+	cy.clearLocalStorage('token')
 	cy.title().should('contain', 'Bookworm')
-	home.checkTitleIsVisible()
-	home.clickOnGotoLoginLink()
+	homePage.checkTitleIsVisible()
+	homePage.clickOnGotoLoginLink()
 	loginPage.checkUrl()
 	loginPage.checkTitleIsVisible()
 	loginPage.enterEmail(email)
