e011faf83c87fdda75fe0b3a63b47dac4aedb868
Willem Verbuyst <willemverbuyst@gmail.com>
2021-12-28 13:38:57 +0100

    Add tabs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,26 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Books from './pages/books';
-import Login from './pages/login';
-import Root from './pages/root';
-import PageNotFound from './pages/pageNotFound';
-import AppBar from './components/AppBar';
-import { ReactElement } from 'react';
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Books from './pages/books'
+import Login from './pages/login'
+import Root from './pages/root'
+import PageNotFound from './pages/pageNotFound'
+import AppBar from './components/AppBar'
+import { ReactElement } from 'react'
 
 const App: React.FC = (): ReactElement => {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <AppBar />
-        <Routes>
-          <Route path="/" element={<Root />} />
-          <Route path="/books" element={<Books />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-};
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<AppBar />
+				<Routes>
+					<Route path="/" element={<Root />} />
+					<Route path="/books" element={<Books />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	)
+}
 
-export default App;
+export default App
--- frontend/src/pages/books/index.tsx
+++ frontend/src/pages/books/index.tsx
@@ -1,32 +1,45 @@
-import { Box } from '@mui/material';
-import React, { ReactElement, useEffect } from 'react';
-import TableWithBooks from '../../components/Table';
-import { useAppState, useActions } from '../../overmind';
-import { useNavigate } from 'react-router-dom';
+import { Box } from '@mui/material'
+import React, { ReactElement, useEffect } from 'react'
+import TableWithBooks from '../../components/Table'
+import { useAppState, useActions } from '../../overmind'
+import { useNavigate } from 'react-router-dom'
+import BasicTabs from '../../components/BasicTabs'
 
 const Books: React.FC = (): ReactElement => {
-  const navigate = useNavigate();
-  const { allBooks, isLoggedIn } = useAppState();
-  const { fetchAllBooks } = useActions();
+	const navigate = useNavigate()
+	const { allBooks, isLoggedIn } = useAppState()
+	const { fetchAllBooks } = useActions()
 
-  useEffect(() => {
-    if (!isLoggedIn) {
-      navigate('/');
-    }
-  });
+	useEffect(() => {
+		if (!isLoggedIn) {
+			navigate('/')
+		}
+	})
 
-  useEffect(() => {
-    if (!allBooks) {
-      fetchAllBooks();
-    }
-  });
+	useEffect(() => {
+		if (!allBooks) {
+			fetchAllBooks()
+		}
+	})
 
-  return (
-    <Box>
-      <h1 className="title">Books</h1>
-      {allBooks ? <TableWithBooks books={allBooks} /> : <p>No books</p>}
-    </Box>
-  );
-};
+	return (
+		<>
+			<BasicTabs
+				overview={
+					<Box>
+						<h1 className="title">Books</h1>
+						{allBooks ? <TableWithBooks books={allBooks} /> : <p>No books</p>}
+					</Box>
+				}
+				statistics={
+					<Box>
+						<h1 className="title">Books</h1>
+						<p>some charts</p>
+					</Box>
+				}
+			/>
+		</>
+	)
+}
 
-export default Books;
+export default Books
