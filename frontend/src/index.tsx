682609601c6ac77cba6815954c357872e47384d3
Willem Verbuyst <willemverbuyst@gmail.com>
2022-01-06 18:57:41 +0100

    Format code

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 // import reportWebVitals from './reportWebVitals';
-import { createOvermind } from 'overmind';
-import { Provider } from 'overmind-react';
-import { config } from './overmind';
-import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
-import { ThemeProvider } from '@mui/material';
-import theme from './theme/colors';
+import { createOvermind } from 'overmind'
+import { Provider } from 'overmind-react'
+import { config } from './overmind'
+import ScopedCssBaseline from '@mui/material/ScopedCssBaseline'
+import { ThemeProvider } from '@mui/material'
+import theme from './theme/colors'
 
-const overmind = createOvermind(config);
+const overmind = createOvermind(config)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider value={overmind}>
-      <ScopedCssBaseline>
-        <ThemeProvider theme={theme}>
-          <App />
-        </ThemeProvider>
-      </ScopedCssBaseline>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<React.StrictMode>
+		<Provider value={overmind}>
+			<ScopedCssBaseline>
+				<ThemeProvider theme={theme}>
+					<App />
+				</ThemeProvider>
+			</ScopedCssBaseline>
+		</Provider>
+		,
+	</React.StrictMode>,
+	document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
