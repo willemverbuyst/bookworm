a0c41178ec9f79313ba7c2be1a451b8ef2cb256a
Willem Verbuyst <willemverbuyst@gmail.com>
2022-01-06 18:52:10 +0100

    Update axios calls

--- frontend/src/overmind/effects.ts
+++ frontend/src/overmind/effects.ts
@@ -5,17 +5,17 @@ import { UserApi } from '../models/User'
 
 export const api = {
 	getAllAuthors: async (): Promise<AuthorApi> => {
-		const response = await axios.get(`http://0.0.0.0:8000/authors`)
+		const response = await axios.get(`http://localhost:8000/authors`)
 		return response.data
 	},
 
 	getAllBooks: async (): Promise<BookApi> => {
-		const response = await axios.get(`http://0.0.0.0:8000/books`)
+		const response = await axios.get(`http://localhost:8000/books`)
 		return response.data
 	},
 
 	getUser: async (email: string, password: string): Promise<UserApi> => {
-		const response = await axios.post(`http://0.0.0.0:8000/login`, {
+		const response = await axios.post(`http://localhost:8000/login`, {
 			email,
 			password,
 		})
