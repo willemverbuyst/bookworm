0521753d4d27db1cdfa2bc5378233cd9c9c6c4be
Willem Verbuyst <willemverbuyst@gmail.com>
2022-01-08 05:22:28 +0100

    Update feedback page

--- frontend/src/pages/feedback/index.tsx
+++ frontend/src/pages/feedback/index.tsx
@@ -1,13 +1,31 @@
+import {
+	IonContent,
+	IonPage,
+	IonText,
+	IonTitle,
+	IonToolbar,
+} from '@ionic/react'
 import React, { ReactElement } from 'react'
 import { useAppState } from '../../overmind'
 
 const Feedback: React.FC = (): ReactElement => {
 	const { isLoggedIn } = useAppState()
 
-	return isLoggedIn ? (
-		<h1 className="title">Feedback</h1>
-	) : (
-		<p>you are not logged in</p>
+	return (
+		<IonPage>
+			<IonToolbar>
+				<IonTitle>Feedback</IonTitle>
+			</IonToolbar>
+			<IonContent>
+				{isLoggedIn ? (
+					<></>
+				) : (
+					<IonText>
+						<h4>you are not logged in</h4>
+					</IonText>
+				)}
+			</IonContent>
+		</IonPage>
 	)
 }
 
