6f2114d9236e040e7b9446c65d1e9d5d37b43f08
Willem Verbuyst <willemverbuyst@gmail.com>
2022-01-09 15:18:10 +0100

    Style error message and form

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -3,7 +3,14 @@ import { useForm, Controller, SubmitHandler } from 'react-hook-form'
 import { Box, Button } from '@mui/material'
 import TextField from '@mui/material/TextField'
 import { useAppState, useActions } from '../../overmind'
-import { IonContent, IonPage } from '@ionic/react'
+import {
+	IonCol,
+	IonContent,
+	IonGrid,
+	IonPage,
+	IonRow,
+	IonText,
+} from '@ionic/react'
 import { useHistory } from 'react-router-dom'
 import ToolBar from '../../components/ToolBar'
 
@@ -26,63 +33,75 @@ const Login: React.FC = () => {
 	}
 
 	const displayErrorMessage = () => (
-		<p className="error">{appErrors.loginForm}</p>
+		<IonRow>
+			<IonCol class="ion-text-center">
+				<IonText color="danger">
+					<p className="error">{appErrors.loginForm}</p>
+				</IonText>
+			</IonCol>
+		</IonRow>
 	)
 
 	return (
 		<IonPage>
 			<ToolBar title="Login" showLoginBtn={false} />
 			<IonContent>
-				<form onSubmit={handleSubmit(onSubmit)}>
-					<Box
-						sx={{
-							display: 'flex',
-							flexDirection: 'column',
-							alignItems: 'center',
-							overflow: 'hidden',
-							margin: 'auto',
-						}}
-					>
-						<Box sx={{ marginTop: 3 }}>
-							<Controller
-								name="email"
-								control={control}
-								rules={{ required: true }}
-								defaultValue=""
-								render={({ field }) => (
-									<TextField
-										{...field}
-										id="outlined-required"
-										label="email"
-										type="email"
-									/>
-								)}
-							/>
-						</Box>
-						<Box sx={{ marginTop: 3 }}>
-							<Controller
-								name="password"
-								control={control}
-								rules={{ required: true }}
-								defaultValue=""
-								render={({ field }) => (
-									<TextField
-										{...field}
-										id="outlined-required"
-										label="password"
-										type="password"
-									/>
-								)}
-							/>
-						</Box>
-						<Box sx={{ marginTop: 3 }}>
-							<Button type="submit" variant="contained">
-								LOG IN
-							</Button>
-						</Box>
-					</Box>
-				</form>
-				{appErrors.loginForm ? displayErrorMessage() : null}
+				<IonGrid>
+					<IonRow>
+						<IonCol>
+							<form onSubmit={handleSubmit(onSubmit)}>
+								<Box
+									sx={{
+										display: 'flex',
+										flexDirection: 'column',
+										alignItems: 'center',
+										overflow: 'hidden',
+										margin: 'auto',
+									}}
+								>
+									<Box sx={{ marginTop: 3 }}>
+										<Controller
+											name="email"
+											control={control}
+											rules={{ required: true }}
+											defaultValue=""
+											render={({ field }) => (
+												<TextField
+													{...field}
+													id="outlined-required"
+													label="email"
+													type="email"
+												/>
+											)}
+										/>
+									</Box>
+									<Box sx={{ marginTop: 3 }}>
+										<Controller
+											name="password"
+											control={control}
+											rules={{ required: true }}
+											defaultValue=""
+											render={({ field }) => (
+												<TextField
+													{...field}
+													id="outlined-required"
+													label="password"
+													type="password"
+												/>
+											)}
+										/>
+									</Box>
+									<Box sx={{ marginTop: 3 }}>
+										<Button type="submit" variant="contained">
+											LOG IN
+										</Button>
+									</Box>
+								</Box>
+							</form>
+						</IonCol>
+					</IonRow>
+					{appErrors.loginForm ? displayErrorMessage() : null}
+				</IonGrid>
 			</IonContent>
 		</IonPage>
 	)
