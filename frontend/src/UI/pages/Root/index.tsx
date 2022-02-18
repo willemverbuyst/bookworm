becdce4d3ce8adee1c072abc2c83da1b0dd6e4c3
Willem Verbuyst <willemverbuyst@gmail.com>
2022-02-18 05:28:46 +0100

    Work on homepage

--- frontend/src/UI/pages/Root/index.tsx
+++ frontend/src/UI/pages/Root/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from '@mui/material'
+import { Box, Card, CardMedia, Container, Typography } from '@mui/material'
 import React, { ReactElement } from 'react'
 
 const Root: React.FC = (): ReactElement => {
@@ -8,8 +8,34 @@ const Root: React.FC = (): ReactElement => {
 				<Typography variant="h2">Home</Typography>
 			</Box>
 			<Box>
-				<Box sx={{ m: 5, textAlign: 'center' }}>
-					<Typography variant="h3">Welcome!</Typography>
+				<Box sx={{ display: 'flex', justifyContent: 'center' }}>
+					<Card>
+						<div style={{ position: 'relative' }}>
+							<CardMedia
+								style={{ width: '100%' }}
+								component="img"
+								image="https://images.unsplash.com/photo-1495446815901-a7297e633e8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+								title="Pancakes"
+								alt="Pancakes"
+							/>
+							<div
+								style={{
+									position: 'absolute',
+									color: 'white',
+									bottom: 10,
+									left: '50%',
+									transform: 'translateX(-50%)',
+									width: '100%',
+									textAlign: 'center',
+								}}
+							>
+								<Typography sx={{ letterSpacing: 2 }}>
+									Welcome bookworm, have a look at our library and leave a book
+									review.
+								</Typography>
+							</div>
+						</div>
+					</Card>
 				</Box>
 			</Box>
 		</Container>
