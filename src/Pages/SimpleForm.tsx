930b208917d88c22e07e31f1aa0e20c3da9cc445
Willem Verbuyst <willemverbuyst@gmail.com>
2022-10-23 09:07:53 +0200

    feat: add validation

--- src/Pages/SimpleForm.tsx
+++ src/Pages/SimpleForm.tsx
@@ -1,4 +1,4 @@
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler } from 'react-hook-form'
 
 enum GenderEnum {
   female = 'female',
@@ -7,25 +7,49 @@ enum GenderEnum {
 }
 
 interface IFormInput {
-  firstName: String;
-  gender: GenderEnum;
+  firstName: String
+  gender: GenderEnum
+  age: number
 }
 
 export default function SimpleForm() {
-  const { register, handleSubmit } = useForm<IFormInput>();
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IFormInput>()
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <label>First Name</label>
-      <input {...register('firstName')} />
-      <label>Gender Selection</label>
+      <input
+        {...register('firstName', { required: 'first name is required' })}
+      />
+      {errors.firstName?.type === 'required' && (
+        <p role="alert" className="error">
+          First name is required
+        </p>
+      )}
+      <label>Gender</label>
       <select {...register('gender')}>
         <option value="female">female</option>
         <option value="male">male</option>
         <option value="other">other</option>
       </select>
-      <input type="submit" />
+      <label>Age</label>
+      <input type="number" {...register('age', { min: 18, max: 99 })} />
+      {errors.age?.type === 'min' && (
+        <p role="alert" className="error">
+          Minimum age is 18
+        </p>
+      )}
+      {errors.age?.type === 'max' && (
+        <p role="alert" className="error">
+          Maximum age is 99
+        </p>
+      )}
+      <button type="submit">submit</button>
     </form>
-  );
+  )
 }
