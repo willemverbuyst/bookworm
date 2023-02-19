c5011338b30cee4b4eab7d3e38f2bb3cb05381a2
Willem Verbuyst <willemverbuyst@gmail.com>
2023-02-19 17:09:10 +0100

    Set up nav bar

--- frontend/src/UI/components/AppBar/index.tsx
+++ frontend/src/UI/components/AppBar/index.tsx
@@ -1,127 +1,383 @@
-import * as React from "react";
-import { useNavigate } from "react-router-dom";
-import { useActions, useAppState } from "../../../business/overmind";
+/* eslint-disable react/jsx-props-no-spreading */
+// import * as React from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useActions, useAppState } from "../../../business/overmind";
 
-const pages = ["home", "books", "authors", "review"];
+// const pages = ["home", "books", "authors", "review"];
 
-const ResponsiveAppBar = () => {
-  const navigate = useNavigate();
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
+// const ResponsiveAppBar = () => {
+//   const navigate = useNavigate();
+//   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+//     null
+//   );
+
+//   const { logoutUser } = useActions();
+//   const { isLoggedIn } = useAppState();
+
+//   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+//     setAnchorElNav(event.currentTarget);
+//   };
+
+//   const handleCloseNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+//     setAnchorElNav(null);
+//     navigate(`/${event.currentTarget.textContent}`);
+//   };
+
+//   const handleLogout = () => logoutUser();
+
+//   const handleLogin = () => navigate("/login");
+
+//   return (
+//     <p>app bar</p>
+// <AppBar position="static">
+//   <Container maxWidth="xl">
+//     <Toolbar disableGutters>
+//       <Typography
+//         variant="h6"
+//         noWrap
+//         component="div"
+//         sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+//       >
+//         BOOKWORM
+//       </Typography>
+
+//       <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+//         <IconButton
+//           size="large"
+//           aria-label="account of current user"
+//           aria-controls="menu-appbar"
+//           aria-haspopup="true"
+//           onClick={handleOpenNavMenu}
+//           color="inherit"
+//         >
+//           <MenuIcon />
+//         </IconButton>
+//         <Menu
+//           id="menu-appbar"
+//           anchorEl={anchorElNav}
+//           anchorOrigin={{
+//             vertical: "bottom",
+//             horizontal: "left",
+//           }}
+//           keepMounted
+//           transformOrigin={{
+//             vertical: "top",
+//             horizontal: "left",
+//           }}
+//           open={Boolean(anchorElNav)}
+//           onClose={handleCloseNavMenu}
+//           sx={{
+//             display: { xs: "block", md: "none" },
+//           }}
+//         >
+//           {pages.map((page) => (
+//             <MenuItem key={page} onClick={handleCloseNavMenu}>
+//               <Typography textAlign="center">{page}</Typography>
+//             </MenuItem>
+//           ))}
+//         </Menu>
+//       </Box>
+//       <Typography
+//         variant="h6"
+//         noWrap
+//         component="div"
+//         sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+//       >
+//         BOOKWORM
+//       </Typography>
+//       <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+//         {pages.map((page) => (
+//           <Button
+//             key={page}
+//             onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) =>
+//               handleCloseNavMenu(e)
+//             }
+//             sx={{ my: 2, color: "white", display: "block" }}
+//           >
+//             {page}
+//           </Button>
+//         ))}
+//       </Box>
+
+//       <Box>
+//         {isLoggedIn ? (
+//           <Button
+//             id="logout-btn"
+//             key="logout-btn"
+//             onClick={handleLogout}
+//             sx={{ my: 2, color: "white", display: "block" }}
+//           >
+//             LOG OUT
+//           </Button>
+//         ) : (
+//           <Button
+//             id="login-btn"
+//             key="login-btn"
+//             onClick={handleLogin}
+//             sx={{ my: 2, color: "white", display: "block" }}
+//           >
+//             LOG IN
+//           </Button>
+//         )}
+//       </Box>
+//     </Toolbar>
+//   </Container>
+// </AppBar>
+//   );
+// };
+// export default ResponsiveAppBar;
+import React from "react";
+import {
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Button,
+  Stack,
+  Collapse,
+  Icon,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useColorModeValue,
+  useBreakpointValue,
+  useDisclosure,
+} from "@chakra-ui/react";
+import {
+  HamburgerIcon,
+  CloseIcon,
+  ChevronDownIcon,
+  ChevronRightIcon,
+} from "@chakra-ui/icons";
+import { NavItem, NAV_ITEMS } from "./navItems";
+
+function DesktopSubNav({ label, href, subLabel }: NavItem) {
+  return (
+    <Link
+      href={href}
+      role="group"
+      display="block"
+      p={2}
+      rounded="md"
+      _hover={{ bg: useColorModeValue("pink.50", "gray.900") }}
+    >
+      <Stack direction="row" align="center">
+        <Box>
+          <Text
+            transition="all .3s ease"
+            _groupHover={{ color: "pink.400" }}
+            fontWeight={500}
+          >
+            {label}
+          </Text>
+          <Text fontSize="sm">{subLabel}</Text>
+        </Box>
+        <Flex
+          transition="all .3s ease"
+          transform="translateX(-10px)"
+          opacity={0}
+          _groupHover={{ opacity: "100%", transform: "translateX(0)" }}
+          justify="flex-end"
+          align="center"
+          flex={1}
+        >
+          <Icon color="pink.400" w={5} h={5} as={ChevronRightIcon} />
+        </Flex>
+      </Stack>
+    </Link>
   );
+}
 
-  const { logoutUser } = useActions();
-  const { isLoggedIn } = useAppState();
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(null);
-    navigate(`/${event.currentTarget.textContent}`);
-  };
-
-  const handleLogout = () => logoutUser();
-
-  const handleLogin = () => navigate("/login");
+function MobileNavItem({ label, children, href }: NavItem) {
+  const { isOpen, onToggle } = useDisclosure();
 
   return (
-    <p>app bar</p>
-    // <AppBar position="static">
-    //   <Container maxWidth="xl">
-    //     <Toolbar disableGutters>
-    //       <Typography
-    //         variant="h6"
-    //         noWrap
-    //         component="div"
-    //         sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
-    //       >
-    //         BOOKWORM
-    //       </Typography>
+    <Stack spacing={4} onClick={children && onToggle}>
+      <Flex
+        py={2}
+        as={Link}
+        href={href ?? "#"}
+        justify="space-between"
+        align="center"
+        _hover={{
+          textDecoration: "none",
+        }}
+      >
+        <Text
+          fontWeight={600}
+          color={useColorModeValue("gray.600", "gray.200")}
+        >
+          {label}
+        </Text>
+        {children && (
+          <Icon
+            as={ChevronDownIcon}
+            transition="all .25s ease-in-out"
+            transform={isOpen ? "rotate(180deg)" : ""}
+            w={6}
+            h={6}
+          />
+        )}
+      </Flex>
 
-    //       <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-    //         <IconButton
-    //           size="large"
-    //           aria-label="account of current user"
-    //           aria-controls="menu-appbar"
-    //           aria-haspopup="true"
-    //           onClick={handleOpenNavMenu}
-    //           color="inherit"
-    //         >
-    //           <MenuIcon />
-    //         </IconButton>
-    //         <Menu
-    //           id="menu-appbar"
-    //           anchorEl={anchorElNav}
-    //           anchorOrigin={{
-    //             vertical: "bottom",
-    //             horizontal: "left",
-    //           }}
-    //           keepMounted
-    //           transformOrigin={{
-    //             vertical: "top",
-    //             horizontal: "left",
-    //           }}
-    //           open={Boolean(anchorElNav)}
-    //           onClose={handleCloseNavMenu}
-    //           sx={{
-    //             display: { xs: "block", md: "none" },
-    //           }}
-    //         >
-    //           {pages.map((page) => (
-    //             <MenuItem key={page} onClick={handleCloseNavMenu}>
-    //               <Typography textAlign="center">{page}</Typography>
-    //             </MenuItem>
-    //           ))}
-    //         </Menu>
-    //       </Box>
-    //       <Typography
-    //         variant="h6"
-    //         noWrap
-    //         component="div"
-    //         sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
-    //       >
-    //         BOOKWORM
-    //       </Typography>
-    //       <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-    //         {pages.map((page) => (
-    //           <Button
-    //             key={page}
-    //             onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) =>
-    //               handleCloseNavMenu(e)
-    //             }
-    //             sx={{ my: 2, color: "white", display: "block" }}
-    //           >
-    //             {page}
-    //           </Button>
-    //         ))}
-    //       </Box>
-
-    //       <Box>
-    //         {isLoggedIn ? (
-    //           <Button
-    //             id="logout-btn"
-    //             key="logout-btn"
-    //             onClick={handleLogout}
-    //             sx={{ my: 2, color: "white", display: "block" }}
-    //           >
-    //             LOG OUT
-    //           </Button>
-    //         ) : (
-    //           <Button
-    //             id="login-btn"
-    //             key="login-btn"
-    //             onClick={handleLogin}
-    //             sx={{ my: 2, color: "white", display: "block" }}
-    //           >
-    //             LOG IN
-    //           </Button>
-    //         )}
-    //       </Box>
-    //     </Toolbar>
-    //   </Container>
-    // </AppBar>
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: "0!important" }}>
+        <Stack
+          mt={2}
+          pl={4}
+          borderLeft={1}
+          borderStyle="solid"
+          borderColor={useColorModeValue("gray.200", "gray.700")}
+          align="start"
+        >
+          {children &&
+            children.map((child) => (
+              <Link key={child.label} py={2} href={child.href}>
+                {child.label}
+              </Link>
+            ))}
+        </Stack>
+      </Collapse>
+    </Stack>
   );
-};
-export default ResponsiveAppBar;
+}
+
+function MobileNav() {
+  return (
+    <Stack
+      bg={useColorModeValue("white", "gray.800")}
+      p={4}
+      display={{ md: "none" }}
+    >
+      {NAV_ITEMS.map((navItem) => (
+        <MobileNavItem key={navItem.label} {...navItem} />
+      ))}
+    </Stack>
+  );
+}
+
+function DesktopNav() {
+  const linkColor = useColorModeValue("gray.600", "gray.200");
+  const linkHoverColor = useColorModeValue("gray.800", "white");
+  const popoverContentBgColor = useColorModeValue("white", "gray.800");
+
+  return (
+    <Stack direction="row" spacing={4}>
+      {NAV_ITEMS.map((navItem) => (
+        <Box key={navItem.label}>
+          <Popover trigger="hover" placement="bottom-start">
+            <PopoverTrigger>
+              <Link
+                p={2}
+                href={navItem.href ?? "#"}
+                fontSize="sm"
+                fontWeight={500}
+                color={linkColor}
+                _hover={{
+                  textDecoration: "none",
+                  color: linkHoverColor,
+                }}
+              >
+                {navItem.label}
+              </Link>
+            </PopoverTrigger>
+
+            {navItem.children && (
+              <PopoverContent
+                border={0}
+                boxShadow="xl"
+                bg={popoverContentBgColor}
+                p={4}
+                rounded="xl"
+                minW="sm"
+              >
+                <Stack>
+                  {navItem.children.map((child) => (
+                    <DesktopSubNav key={child.label} {...child} />
+                  ))}
+                </Stack>
+              </PopoverContent>
+            )}
+          </Popover>
+        </Box>
+      ))}
+    </Stack>
+  );
+}
+
+export default function WithSubnavigation() {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Box>
+      <Flex
+        bg={useColorModeValue("white", "gray.800")}
+        color={useColorModeValue("gray.600", "white")}
+        minH="60px"
+        py={{ base: 2 }}
+        px={{ base: 4 }}
+        borderBottom={1}
+        borderStyle="solid"
+        borderColor={useColorModeValue("gray.200", "gray.900")}
+        align="center"
+      >
+        <Flex
+          flex={{ base: 1, md: "auto" }}
+          ml={{ base: -2 }}
+          display={{ base: "flex", md: "none" }}
+        >
+          <IconButton
+            onClick={onToggle}
+            icon={
+              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+            }
+            variant="ghost"
+            aria-label="Toggle Navigation"
+          />
+        </Flex>
+        <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
+          <Text
+            textAlign={useBreakpointValue({ base: "center", md: "left" })}
+            fontFamily="heading"
+            color={useColorModeValue("gray.800", "white")}
+          >
+            BookWorm
+          </Text>
+
+          <Flex display={{ base: "none", md: "flex" }} ml={10}>
+            <DesktopNav />
+          </Flex>
+        </Flex>
+
+        <Stack
+          flex={{ base: 1, md: 0 }}
+          justify="flex-end"
+          direction="row"
+          spacing={6}
+        >
+          <Button as="a" fontSize="sm" fontWeight={400} variant="link" href="#">
+            Sign In
+          </Button>
+          <Button
+            as="a"
+            display={{ base: "none", md: "inline-flex" }}
+            fontSize="sm"
+            fontWeight={600}
+            color="white"
+            bg="pink.400"
+            href="#"
+            _hover={{
+              bg: "pink.300",
+            }}
+          >
+            Sign Up
+          </Button>
+        </Stack>
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity>
+        <MobileNav />
+      </Collapse>
+    </Box>
+  );
+}
---
+++ frontend/src/UI/components/AppBar/navItems.ts
@@ -0,0 +1,13 @@
+export interface NavItem {
+  label: string;
+  subLabel?: string;
+  children?: Array<NavItem>;
+  href?: string;
+}
+
+export const NAV_ITEMS: Array<NavItem> = [
+  { label: "Home", href: "#" },
+  { label: "Books", href: "#" },
+  { label: "Authors", href: "#" },
+  { label: "Review", href: "#" },
+];
