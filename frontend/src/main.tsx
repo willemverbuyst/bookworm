b8220e90364201a3388d735a3fe5f5cddbadcffd
Willem Verbuyst <willemverbuyst@gmail.com>
2023-02-20 05:12:27 +0100

    Work on vite config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-// import reportWebVitals from './reportWebVitals';
 import { createOvermind } from "overmind";
 import { Provider } from "overmind-react";
 import { BrowserRouter } from "react-router-dom";
@@ -11,7 +10,7 @@ import App from "./App";
 
 const overmind = createOvermind(config);
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <Provider value={overmind}>
@@ -20,11 +19,5 @@ ReactDOM.render(
         </BrowserRouter>
       </Provider>
     </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   return defineConfig({
-    plugins: [react(), viteTsconfigPaths()],
+    plugins: [react()],
     define: {
       "process.env.NODE_ENV": `"${mode}"`,
     },
