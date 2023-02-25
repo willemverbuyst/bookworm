67ff1d2fdbd7b3000617e5689ad10bdcec639aa1
Willem Verbuyst <willemverbuyst@gmail.com>
2023-02-25 08:18:51 +0100

    Add controlled inputs

---
+++ frontend/src/UI/components/Controllers/NumberInput.tsx
@@ -0,0 +1,58 @@
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from "@chakra-ui/react";
+import {
+  Controller,
+  FieldError,
+  FieldValues,
+  UseControllerProps,
+} from "react-hook-form";
+
+type NumberInputProps<T extends FieldValues> = {
+  error?: FieldError | undefined;
+  helperText?: string | null;
+  label?: string;
+  name: string;
+} & UseControllerProps<T>;
+
+export function ControlledNumberInput<T extends FieldValues>({
+  control,
+  error,
+  helperText,
+  label,
+  name,
+}: NumberInputProps<T>) {
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel style={{ color: "#a3a3a3", fontStyle: "italic" }}>
+        {label}
+      </FormLabel>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <NumberInput
+            {...field}
+            onChange={(value) => field.onChange(Number(value))}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        )}
+      />
+      {error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+      <FormHelperText>{helperText}</FormHelperText>
+    </FormControl>
+  );
+}
