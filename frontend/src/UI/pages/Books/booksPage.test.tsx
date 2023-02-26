edfadcdb315e9656a0554483202ecf8cc0f441c4
Willem Verbuyst <willemverbuyst@gmail.com>
2023-02-26 05:01:34 +0100

    Clean up books page test

--- frontend/src/UI/pages/Books/booksPage.test.tsx
+++ frontend/src/UI/pages/Books/booksPage.test.tsx
@@ -36,7 +36,7 @@ describe("Books page", () => {
     expect(heading).toBeInTheDocument();
   });
 
-  test("should display a table with column headers", () => {
+  test("should display a table", () => {
     const overmind = createOvermindMock(config, (state) => {
       // eslint-disable-next-line no-param-reassign
       state.booksApi = {
@@ -64,66 +64,5 @@ describe("Books page", () => {
       name: /overview of books/i,
     });
     expect(table).toBeInTheDocument();
-
-    const title = screen.getByText(/overview of books/i);
-    expect(title).toBeInTheDocument();
-
-    const columnHeaders = ["title", "language", "author", "year", "read"];
-
-    columnHeaders.forEach((header) => {
-      const columnHeader = screen.getByRole("columnheader", {
-        name: new RegExp(`${header}`),
-      });
-      expect(columnHeader).toBeInTheDocument();
-    });
-  });
-
-  test("should display a table with two rows", () => {
-    const data = [
-      {
-        id: "abc123",
-        title: "test_title_one",
-        language: "test_language_one",
-        author: "test_author_one",
-        year: 1900,
-        read: 1,
-      },
-      {
-        id: "xyz789",
-        title: "test_title_two",
-        language: "test_language_two",
-        author: "test_author_two",
-        year: 2000,
-        read: 2,
-      },
-    ];
-    const overmind = createOvermindMock(config, (state) => {
-      // eslint-disable-next-line no-param-reassign
-      state.booksApi = {
-        status: "ok",
-        data,
-        message: "testing",
-      };
-    });
-    render(
-      <Provider value={overmind}>
-        <BooksPage />
-      </Provider>
-    );
-
-    data
-      .map((d) => {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { id, ...rest } = d;
-        return rest;
-      })
-      .forEach((d) => {
-        Object.values(d).forEach((value) => {
-          const cell = screen.getByRole("cell", {
-            name: new RegExp(`${value}`),
-          });
-          expect(cell).toBeInTheDocument();
-        });
-      });
   });
 });
