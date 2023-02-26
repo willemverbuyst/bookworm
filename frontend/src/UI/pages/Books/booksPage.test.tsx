88dc09223be6571a1a4de3c3aa30b010f1050358
Willem Verbuyst <willemverbuyst@gmail.com>
2023-02-26 04:52:26 +0100

    Test happy path books page

--- frontend/src/UI/pages/Books/booksPage.test.tsx
+++ frontend/src/UI/pages/Books/booksPage.test.tsx
@@ -1,14 +1,129 @@
 import { render, screen } from "@testing-library/react";
+import { createOvermindMock } from "overmind";
+import { Provider } from "overmind-react";
 import BooksPage from "./booksPage";
+import { config } from "../../../business/overmind";
 
 describe("Books page", () => {
   test("should display a title", () => {
-    render(<BooksPage />);
+    const overmind = createOvermindMock(config, (state) => {
+      // eslint-disable-next-line no-param-reassign
+      state.booksApi = {
+        status: "ok",
+        data: [
+          {
+            id: "abc123",
+            title: "test_title_one",
+            language: "test_language_one",
+            author: "test_author_one",
+            year: 1900,
+            read: 1,
+          },
+        ],
+        message: "testing",
+      };
+    });
+    render(
+      <Provider value={overmind}>
+        <BooksPage />
+      </Provider>
+    );
 
     const heading = screen.getByRole("heading", {
       name: /books/i,
     });
 
     expect(heading).toBeInTheDocument();
+  });
+
+  test("should display a table with column headers", () => {
+    const overmind = createOvermindMock(config, (state) => {
+      // eslint-disable-next-line no-param-reassign
+      state.booksApi = {
+        status: "ok",
+        data: [
+          {
+            id: "abc123",
+            title: "test_title_one",
+            language: "test_language_one",
+            author: "test_author_one",
+            year: 1900,
+            read: 1,
+          },
+        ],
+        message: "testing",
+      };
+    });
+    render(
+      <Provider value={overmind}>
+        <BooksPage />
+      </Provider>
+    );
+
+    const table = screen.getByRole("table", {
+      name: /overview of books/i,
+    });
+    expect(table).toBeInTheDocument();
+
+    const title = screen.getByText(/overview of books/i);
+    expect(title).toBeInTheDocument();
+
+    const columnHeaders = ["title", "language", "author", "year", "read"];
+
+    columnHeaders.forEach((header) => {
+      const columnHeader = screen.getByRole("columnheader", {
+        name: new RegExp(`${header}`),
+      });
+      expect(columnHeader).toBeInTheDocument();
+    });
+  });
+
+  test("should display a table with two rows", () => {
+    const data = [
+      {
+        id: "abc123",
+        title: "test_title_one",
+        language: "test_language_one",
+        author: "test_author_one",
+        year: 1900,
+        read: 1,
+      },
+      {
+        id: "xyz789",
+        title: "test_title_two",
+        language: "test_language_two",
+        author: "test_author_two",
+        year: 2000,
+        read: 2,
+      },
+    ];
+    const overmind = createOvermindMock(config, (state) => {
+      // eslint-disable-next-line no-param-reassign
+      state.booksApi = {
+        status: "ok",
+        data,
+        message: "testing",
+      };
+    });
+    render(
+      <Provider value={overmind}>
+        <BooksPage />
+      </Provider>
+    );
+
+    data
+      .map((d) => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { id, ...rest } = d;
+        return rest;
+      })
+      .forEach((d) => {
+        Object.values(d).forEach((value) => {
+          const cell = screen.getByRole("cell", {
+            name: new RegExp(`${value}`),
+          });
+          expect(cell).toBeInTheDocument();
+        });
+      });
   });
 });
