6d52bfe2e22f177599cfc57352765c97bde575dd
Willem Verbuyst <willemverbuyst@gmail.com>
2023-02-28 19:14:22 +0100

    Write test for form label

---
+++ frontend/src/UI/components/Controllers/Label.test.tsx
@@ -0,0 +1,45 @@
+import { FormControl } from "@chakra-ui/react";
+import { render, screen } from "@testing-library/react";
+import Label from "./Label";
+
+describe("Label", () => {
+  test("should display label text passed as prop", () => {
+    render(
+      <FormControl>
+        <Label text="test label" isRequired={false} />
+      </FormControl>
+    );
+
+    const labelText = screen.getByText(/test label/i);
+
+    expect(labelText).toBeInTheDocument();
+  });
+
+  test("should display label with asterix if required is true", () => {
+    render(
+      <FormControl>
+        <Label text="test label" isRequired />
+      </FormControl>
+    );
+
+    const labelText = screen.getByText(/test label*/i);
+
+    expect(labelText).toBeInTheDocument();
+  });
+
+  test("should display label in grey and italic", () => {
+    render(
+      <FormControl>
+        <Label text="test label" isRequired />
+      </FormControl>
+    );
+
+    const labelText = screen.getByText(/test label*/i);
+
+    expect(labelText).toHaveStyle(
+      "color: rgb(163, 163, 163); font-style: italic;"
+    );
+
+    screen.logTestingPlaygroundURL();
+  });
+});
