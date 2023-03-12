6293c2de1c89b8c7ed1c6d4622adcba7b796e64e
Willem Verbuyst <willemverbuyst@gmail.com>
2023-03-12 05:00:21 +0100

    Handle query params

--- backend/database/python/book/get_books.py
+++ backend/database/python/book/get_books.py
@@ -1,10 +1,9 @@
 import psycopg2
 import os
-from database.python.helpers.sql_helpers import executeScriptsFromFile
 from database.python.helpers.format_data import format_books
 
 dirname = os.path.dirname(__file__)
-select_all_books_sql = os.path.join(dirname, "../../sql/book/select_all_books.sql")
+select_all_books_sql = os.path.join(dirname, "../../sql/book/books_filtered.sql")
 
 DATABASE = os.environ.get("DATABASE")
 DATABASE_USER = os.environ.get("DATABASE_USER")
@@ -23,8 +22,108 @@ def get_books_from_db():
     )
 
     cursor = conn.cursor()
+    cursor.execute("""
+        SELECT 
+            book.book_id,
+            book.title,
+            book.year_published, 
+            CONCAT (author.first_name, ' ', author.last_name) as author,
+            genre.genre, 
+            language.language 
+        FROM book 
+        INNER JOIN book_author 
+        ON book.book_id = book_author.book_id 
+        INNER JOIN author 
+        ON author.author_id = book_author.author_id 
+        INNER JOIN genre 
+        ON genre.genre_id = book.genre_id 
+        INNER JOIN language 
+        ON book.language_id = language.language_id 
+        ORDER BY book.title;
+        """
+    )
 
-    executeScriptsFromFile(select_all_books_sql, cursor)
+    data = cursor.fetchall()
+    conn.close()
+
+    books_formatted = format_books(data)
+
+    return books_formatted
+
+
+def get_books_from_db_by_language(language):
+    conn = psycopg2.connect(
+        database=DATABASE,
+        user=DATABASE_USER,
+        password=DATABASE_PASSWORD,
+        host=DATABASE_HOST,
+        port=DATABASE_PORT,
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT 
+            book.book_id,
+            book.title,
+            book.year_published, 
+            CONCAT (author.first_name, ' ', author.last_name) as author,
+            genre.genre, 
+            language.language 
+        FROM book 
+        INNER JOIN book_author 
+        ON book.book_id = book_author.book_id 
+        INNER JOIN author 
+        ON author.author_id = book_author.author_id 
+        INNER JOIN genre 
+        ON genre.genre_id = book.genre_id 
+        INNER JOIN language 
+        ON book.language_id = language.language_id 
+        WHERE language.language_id = %s 
+        ORDER BY book.title; 
+        """, 
+        (language,)
+    )
+
+    data = cursor.fetchall()
+    conn.close()
+
+    books_formatted = format_books(data)
+
+    return books_formatted
+
+
+def get_books_from_db_by_genre(genre):
+    conn = psycopg2.connect(
+        database=DATABASE,
+        user=DATABASE_USER,
+        password=DATABASE_PASSWORD,
+        host=DATABASE_HOST,
+        port=DATABASE_PORT,
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT 
+            book.book_id,
+            book.title,
+            book.year_published, 
+            CONCAT (author.first_name, ' ', author.last_name) as author,
+            genre.genre, 
+            language.language 
+        FROM book 
+        INNER JOIN book_author 
+        ON book.book_id = book_author.book_id 
+        INNER JOIN author 
+        ON author.author_id = book_author.author_id 
+        INNER JOIN genre 
+        ON genre.genre_id = book.genre_id 
+        INNER JOIN language 
+        ON book.language_id = language.language_id 
+        WHERE genre.genre_id = %s 
+        ORDER BY book.title; 
+        """, 
+        (genre,)
+    )
 
     data = cursor.fetchall()
     conn.close()
--- backend/routers/books.py
+++ backend/routers/books.py
@@ -1,14 +1,26 @@
 from fastapi import APIRouter
 from error.main import raise_exception
-from database.python.book.get_books import get_books_from_db
+from database.python.book.get_books import (
+    get_books_from_db, 
+    get_books_from_db_by_genre, 
+    get_books_from_db_by_language
+)
+
 
 book_router = APIRouter()
 
 
-@book_router.get("/books", tags=["books"])
-def get_all_books() -> dict:
+@book_router.get("/books/", tags=["books"])
+def get_all_books(genre = None, language = None) -> dict:
+
     try:
-        books = get_books_from_db()
+        if genre:
+            books = get_books_from_db_by_genre(genre)
+        elif language:
+            books = get_books_from_db_by_language(language)
+        else:
+            books = get_books_from_db()
+        
         result = len(books)
 
         return {
