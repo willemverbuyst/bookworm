f8fd6c796772de024e1d8e60cd1e8071de559b67
Willem Verbuyst <willemverbuyst@gmail.com>
2023-03-18 09:46:39 +0100

    Fix SignInPage test

--- frontend/src/UI/pages/Auth/SignInPage.test.tsx
+++ frontend/src/UI/pages/Auth/SignInPage.test.tsx
@@ -1,9 +1,35 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { render, screen } from "@testing-library/react";
+import { createOvermindMock } from "overmind";
+import { Provider } from "overmind-react";
+import { BrowserRouter } from "react-router-dom";
+import { vi } from "vitest";
+import { config } from "../../../business/overmind";
 import SignInPage from "./SignInPage";
 
+vi.mock("../../components/Navigation/NavigationBar", () => {
+  const NavigationBar = vi.fn();
+  return { default: NavigationBar };
+});
+
 describe("SingInPage", () => {
+  const overmind = createOvermindMock(config, (state) => {
+    // eslint-disable-next-line no-param-reassign
+    state.auth = {
+      isSignedIn: false,
+      token: "",
+    };
+  });
   test("should display a title", () => {
-    render(<SignInPage />);
+    render(
+      <ChakraProvider>
+        <Provider value={overmind}>
+          <BrowserRouter>
+            <SignInPage />
+          </BrowserRouter>
+        </Provider>
+      </ChakraProvider>
+    );
 
     const heading = screen.getByRole("heading", {
       name: /sign in/i,
