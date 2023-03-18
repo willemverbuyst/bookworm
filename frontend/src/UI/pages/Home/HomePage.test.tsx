d926d7f587c1af20a3aaa2566bd8c17e85d43829
Willem Verbuyst <willemverbuyst@gmail.com>
2023-03-18 09:22:32 +0100

    Fix test for HomePage

--- frontend/src/UI/pages/Home/HomePage.test.tsx
+++ frontend/src/UI/pages/Home/HomePage.test.tsx
@@ -1,9 +1,32 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { render, screen } from "@testing-library/react";
+import { createOvermindMock } from "overmind";
+import { Provider } from "overmind-react";
+import { vi } from "vitest";
 import HomePage from "./HomePage";
+import { config } from "../../../business/overmind";
+
+vi.mock("../../components/Navigation/NavigationBar", () => {
+  const NavigationBar = vi.fn();
+  return { default: NavigationBar };
+});
 
 describe("HomePage", () => {
   test("should display a title", () => {
-    render(<HomePage />);
+    const overmind = createOvermindMock(config, (state) => {
+      // eslint-disable-next-line no-param-reassign
+      state.auth = {
+        isSignedIn: false,
+        token: "",
+      };
+    });
+    render(
+      <ChakraProvider>
+        <Provider value={overmind}>
+          <HomePage />
+        </Provider>
+      </ChakraProvider>
+    );
 
     const heading = screen.getByRole("heading", {
       name: /home/i,
