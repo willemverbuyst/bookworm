afabbfa91efb426f107cab951fc08d9372b5215c
Willem Verbuyst <willemverbuyst@gmail.com>
2023-03-24 05:49:26 +0100

    Separate component for user details

---
+++ frontend/src/UI/components/Cards/UserDetails.tsx
@@ -0,0 +1,123 @@
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  HStack,
+  Spacer,
+  Stack,
+  StackDivider,
+  Text,
+} from "@chakra-ui/react";
+import { User } from "../../../business/models/User";
+
+interface Props {
+  user: User;
+}
+
+function UserDetails({ user }: Props) {
+  return (
+    <Card width={400}>
+      <CardHeader>
+        <Heading size="md">User details</Heading>
+      </CardHeader>
+
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing="4">
+          <HStack>
+            <Box>
+              <Heading size="xs" textTransform="uppercase">
+                first name
+              </Heading>
+              <Text pt="2" fontSize="sm">
+                {user?.first_name}
+              </Text>
+            </Box>
+            <Spacer />
+            <Box textAlign="right">
+              <Heading size="xs" textTransform="uppercase">
+                last name
+              </Heading>
+              <Text pt="2" fontSize="sm">
+                {user?.last_name}
+              </Text>
+            </Box>
+          </HStack>
+          <HStack>
+            <Box>
+              <Heading size="xs" textTransform="uppercase">
+                birth date
+              </Heading>
+              <Text pt="2" fontSize="sm">
+                {user?.birth_date}
+              </Text>
+            </Box>
+            <Spacer />
+            <Box textAlign="right">
+              <Heading size="xs" textTransform="uppercase">
+                place of birth
+              </Heading>
+              <Text pt="2" fontSize="sm">
+                {user?.place_of_birth}
+              </Text>
+            </Box>
+          </HStack>
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              email
+            </Heading>
+            <Text pt="2" fontSize="sm">
+              {user?.email}
+            </Text>
+          </Box>
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              phone number
+            </Heading>
+            <Text pt="2" fontSize="sm">
+              {user?.phone}
+            </Text>
+          </Box>
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              address
+            </Heading>
+            <Text pt="2" fontSize="sm">
+              {user?.address}
+            </Text>
+          </Box>
+          <HStack>
+            <Box>
+              <Heading size="xs" textTransform="uppercase">
+                city
+              </Heading>
+              <Text pt="2" fontSize="sm">
+                {user?.city}
+              </Text>
+            </Box>
+            <Spacer />
+            <Box textAlign="right">
+              <Heading size="xs" textTransform="uppercase">
+                country
+              </Heading>
+              <Text pt="2" fontSize="sm">
+                {user?.country}
+              </Text>
+            </Box>
+          </HStack>
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              library
+            </Heading>
+            <Text pt="2" fontSize="sm">
+              {user?.library_name}
+            </Text>
+          </Box>
+        </Stack>
+      </CardBody>
+    </Card>
+  );
+}
+
+export default UserDetails;
--- frontend/src/UI/pages/Home/HomePage.tsx
+++ frontend/src/UI/pages/Home/HomePage.tsx
@@ -1,17 +1,6 @@
-import {
-  Box,
-  Card,
-  CardBody,
-  CardHeader,
-  Flex,
-  Heading,
-  HStack,
-  Spacer,
-  Stack,
-  StackDivider,
-  Text,
-} from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import { useAppState } from "../../../business/overmind";
+import UserDetails from "../../components/Cards/UserDetails";
 import NavigationBar from "../../components/Navigation/NavigationBar";
 import PageTitle from "../../components/Text/PageTitle";
 
@@ -23,105 +12,7 @@ function HomePage() {
       <NavigationBar />
       <Flex flexDirection="column" alignItems="center">
         <PageTitle title="Home" />
-        <Card width={400}>
-          <CardHeader>
-            <Heading size="md">User details</Heading>
-          </CardHeader>
-
-          <CardBody>
-            <Stack divider={<StackDivider />} spacing="4">
-              <HStack>
-                <Box>
-                  <Heading size="xs" textTransform="uppercase">
-                    first name
-                  </Heading>
-                  <Text pt="2" fontSize="sm">
-                    {user?.first_name}
-                  </Text>
-                </Box>
-                <Spacer />
-                <Box textAlign="right">
-                  <Heading size="xs" textTransform="uppercase">
-                    last name
-                  </Heading>
-                  <Text pt="2" fontSize="sm">
-                    {user?.last_name}
-                  </Text>
-                </Box>
-              </HStack>
-              <HStack>
-                <Box>
-                  <Heading size="xs" textTransform="uppercase">
-                    birth date
-                  </Heading>
-                  <Text pt="2" fontSize="sm">
-                    {user?.birth_date}
-                  </Text>
-                </Box>
-                <Spacer />
-                <Box textAlign="right">
-                  <Heading size="xs" textTransform="uppercase">
-                    place of birth
-                  </Heading>
-                  <Text pt="2" fontSize="sm">
-                    {user?.place_of_birth}
-                  </Text>
-                </Box>
-              </HStack>
-              <Box>
-                <Heading size="xs" textTransform="uppercase">
-                  email
-                </Heading>
-                <Text pt="2" fontSize="sm">
-                  {user?.email}
-                </Text>
-              </Box>
-              <Box>
-                <Heading size="xs" textTransform="uppercase">
-                  phone number
-                </Heading>
-                <Text pt="2" fontSize="sm">
-                  {user?.phone}
-                </Text>
-              </Box>
-              <Box>
-                <Heading size="xs" textTransform="uppercase">
-                  address
-                </Heading>
-                <Text pt="2" fontSize="sm">
-                  {user?.address}
-                </Text>
-              </Box>
-              <HStack>
-                <Box>
-                  <Heading size="xs" textTransform="uppercase">
-                    city
-                  </Heading>
-                  <Text pt="2" fontSize="sm">
-                    {user?.city}
-                  </Text>
-                </Box>
-                <Spacer />
-                <Box textAlign="right">
-                  <Heading size="xs" textTransform="uppercase">
-                    country
-                  </Heading>
-                  <Text pt="2" fontSize="sm">
-                    {user?.country}
-                  </Text>
-                </Box>
-              </HStack>
-              <Box>
-                <Heading size="xs" textTransform="uppercase">
-                  library
-                </Heading>
-                <Text pt="2" fontSize="sm">
-                  {user?.library_name}
-                </Text>
-              </Box>
-            </Stack>
-          </CardBody>
-        </Card>
+        {user ? <UserDetails user={user} /> : null}
       </Flex>
     </>
   );
