f3a202802218ab39e9588946c575f995dbb1c736
Willem Verbuyst <willemverbuyst@gmail.com>
2023-03-27 04:39:32 +0200

    write sql file directly with python

--- backend/database/dummy_data/language.py
+++ backend/database/dummy_data/language.py
@@ -1,21 +1,19 @@
-import csv
 import datetime
 import random
 
 import config
 
-with open('language.csv', 'w', newline='') as file:
-    writer = csv.writer(file, delimiter="|", quoting=csv.QUOTE_NONNUMERIC)
-    header=[
-      "language_id",
-      "language",
-      "last_updated"
-    ]
-    
-    writer.writerow(header)
+with open('insert_language.sql', 'w') as file:
+    insert_statements = ""
     for i in range(len(config.LANGUAGES)):
-      writer.writerow([
-        str(config.UUIDS_LANGUAGES[i]),
-        config.LANGUAGES[i],
-        datetime.datetime.now(),      
-      ])
+        language_id =  str(config.UUIDS_LANGUAGES[i])
+        language = config.LANGUAGES[i]
+        last_updated = datetime.datetime.now() 
+  
+        sql = "INSERT INTO language (language_id, language, last_updated)" \
+            f"VALUES ('{language_id}'::UUID, '{language}', '{last_updated}');\n"
+        insert_statements += sql
+
+    file.write(insert_statements)
+
+
