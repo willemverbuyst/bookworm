d52ddfee3ec41eb9361b4233d88508bb4ed1a9ef
Willem Verbuyst <willemverbuyst@gmail.com>
2023-03-29 04:42:18 +0200

    Clean up

--- backend/database/dummy_data/address.py
+++ backend/database/dummy_data/address.py
@@ -1,37 +1,5 @@
 import csv
 import datetime
-import random
-
-import config
-import faker
-from faker.providers import DynamicProvider
-
-fake = faker.Faker()
-
-with open('address.csv', 'w', newline='') as file:
-    writer = csv.writer(file, delimiter="|", quoting=csv.QUOTE_NONNUMERIC)
-    header=[
-      "address_id", 
-      "address", 
-      "postal_code",
-      "phone",
-      "last_updated",
-      "city_id"
-    ]
-    
-    writer.writerow(header)
-    for i in range(config.ADDRESS):
-      writer.writerow([
-        1 + i, 
-        fake.street_address(),
-        fake.postcode(),
-        fake.phone_number(),
-        datetime.datetime.now(),
-        random.randint(1,config.CITY)
-      ])
-
-import csv
-import datetime
 
 import config
 import faker
