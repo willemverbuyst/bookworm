1dccc9f953751e8fc9dfe64968c552e2f28f5c91
Willem Verbuyst <willemverbuyst@gmail.com>
2023-04-01 08:43:45 +0200

    Rename files with dummy data and ignore

--- backend/database/dummy_data/create_sql_insert_files.py
+++ backend/database/dummy_data/create_sql_insert_files.py
@@ -7,7 +7,7 @@ dirname = os.path.dirname(__file__)
 
 def create_sql_insert_files(dummy_data, name):
     print(f"[INFO] Write to 'insert_{name}.sql'")
-    with open(f'../sql/{name}/insert_{name}.sql', 'w') as file:
+    with open(f'../sql/{name}/dummy_data_insert_{name}.sql', 'w') as file:
         file.write(dummy_data)
     
 
