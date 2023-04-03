f0f9e5c25bfddfe13135d1e2db093c9244e8aea6
Willem Verbuyst <willemverbuyst@gmail.com>
2023-04-03 05:10:17 +0200

    Add logic for return date null

--- backend/database/sql/rental/select_rentals.sql
+++ backend/database/sql/rental/select_rentals.sql
@@ -5,13 +5,14 @@ SELECT
     book.title, 
     CONCAT (author.first_name, ' ', author.last_name) as author 
 FROM rental 
-FULL OUTER JOIN inventory 
+INNER JOIN inventory 
 ON rental.inventory_id = inventory.inventory_id 
-FULL OUTER JOIN book 
+INNER JOIN book 
 ON inventory.book_id = book.book_id 
 INNER JOIN book_author 
 ON book.book_id = book_author.book_id 
 INNER JOIN author 
 ON author.author_id = book_author.author_id 
+ORDER BY rental.return_date DESC, rental.rental_date DESC  
 LIMIT %s 
 OFFSET %s;
