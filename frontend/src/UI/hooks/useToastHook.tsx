b6aab902aaf86cd6f4a5b4d972d75bdf0bd9f150
Willem Verbuyst <willemverbuyst@gmail.com>
2023-04-05 04:50:55 +0200

    Create hook for toast

---
+++ frontend/src/UI/hooks/useToastHook.tsx
@@ -0,0 +1,40 @@
+import { useToast } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+
+type ToastState = {
+  title: string;
+  description?: string;
+  status: "info" | "warning" | "success" | "error";
+  position?:
+    | "top"
+    | "bottom"
+    | "top-left"
+    | "top-right"
+    | "bottom-left"
+    | "bottom-right";
+};
+
+export function useToastHook() {
+  const [state, setState] = useState<ToastState>({
+    title: "",
+    status: "info",
+  });
+  const toast = useToast();
+
+  useEffect(() => {
+    if (state.title && state.status) {
+      const { title, description, status, position } = state;
+
+      toast({
+        title,
+        description,
+        status,
+        duration: 2500,
+        position: position || "top",
+        isClosable: true,
+      });
+    }
+  }, [state.title, state.status, toast]);
+
+  return [state, setState] as const;
+}
