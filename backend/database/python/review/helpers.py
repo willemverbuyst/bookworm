502b8de9cb7fb4e34324e92c615c38777db282c7
Willem Verbuyst <willemverbuyst@gmail.com>
2023-04-06 03:57:08 +0200

    Update query for reviews

--- backend/database/python/review/helpers.py
+++ backend/database/python/review/helpers.py
@@ -3,6 +3,10 @@ def format_reviews(review_data):
     for row in review_data:
         review = {
             "id": row[0],
+            "description": row[1],
+            "rating": row[2],
+            "book_title": row[3],
+            "reviewer": row[4]
         }
         reviews.append(review)
 
--- backend/database/sql/review/select_reviews.sql
+++ backend/database/sql/review/select_reviews.sql
@@ -1,3 +1,16 @@
 SELECT 
-    review.review_id 
-FROM review;
+    review.review_id, 
+    review.description, 
+    review.rating, 
+    book.title,
+    CONCAT (user_account.first_name, ' ', user_account.last_name) as reviewer  
+FROM review 
+INNER JOIN book 
+ON review.book_id = book.book_id 
+INNER JOIN bookworm 
+ON review.bookworm_id = bookworm.bookworm_id 
+INNER JOIN user_account 
+ON bookworm.user_account_id = user_account.user_account_id
+ORDER BY review.rating DESC  
+LIMIT %s 
+OFFSET %s;
