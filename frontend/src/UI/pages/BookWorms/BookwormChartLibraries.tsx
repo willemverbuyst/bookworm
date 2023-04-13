35c4f1885663e6fdf18fc6bb9ae883c92dbfa967
Willem Verbuyst <willemverbuyst@gmail.com>
2023-04-13 08:56:35 +0200

    Work on display combined pie chart

--- frontend/src/UI/pages/BookWorms/BookwormChartLibraries.tsx
+++ frontend/src/UI/pages/BookWorms/BookwormChartLibraries.tsx
@@ -1,5 +1,6 @@
 import { Box, Spinner } from "@chakra-ui/react";
 import { Cell, Legend, Pie, PieChart } from "recharts";
+import { BookwormStatsLibrary } from "../../../business/models/Bookworm";
 import { useAppState } from "../../../business/overmind";
 import { useGetBookwormStatsLibrary } from "../../hooks/useGetBookwormStatsLibrary";
 
@@ -8,16 +9,51 @@ function BookwormChartLibraries() {
   useGetBookwormStatsLibrary();
   const data = useAppState().bookworm.statsLibrary || [];
 
-  const dataForChart = data.map((d) => ({
-    libraryName: `${d.library_name} ${d.user_active}`,
+  function sumItems(items: BookwormStatsLibrary[]): BookwormStatsLibrary[] {
+    const result: { [library_name: string]: BookwormStatsLibrary } = {};
+    items.reduce((acc, item) => {
+      if (item.library_name in acc) {
+        acc[item.library_name].number_of_bookworms_per_library +=
+          item.number_of_bookworms_per_library;
+      } else {
+        acc[item.library_name] = { ...item };
+      }
+      return acc;
+    }, result);
+    return Object.values(result);
+  }
+
+  function sumItems2(items: BookwormStatsLibrary[]): BookwormStatsLibrary[] {
+    const result: { [user_active: string]: BookwormStatsLibrary } = {};
+    items.reduce((acc, item) => {
+      if (String(item.user_active) in acc) {
+        acc[String(item.user_active)].number_of_bookworms_per_library +=
+          item.number_of_bookworms_per_library;
+      } else {
+        acc[String(item.user_active)] = { ...item };
+      }
+      return acc;
+    }, result);
+    return Object.values(result);
+  }
+
+  const dataForChart = sumItems(data).map((d) => ({
+    libraryName: `${d.library_name}`,
     numberOfBookworms: d.number_of_bookworms_per_library,
   }));
+
+  const dataForChart2 = sumItems2(data).map((d) => ({
+    userIsActive: `${d.user_active}`,
+    numberOfBookworms: d.number_of_bookworms_per_library,
+  }));
+
+  console.log("dataForChart, dataForChart2 :>> ", dataForChart, dataForChart2);
 
   if (isLoading) {
     return <Spinner />;
   }
 
-  const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+  const colors = ["#0088FE", "#FFBB28"];
 
   return (
     <Box>
@@ -26,12 +62,29 @@ function BookwormChartLibraries() {
           <PieChart width={400} height={400}>
             <Legend verticalAlign="bottom" height={36} />
             <Pie
+              data={dataForChart2}
+              dataKey="numberOfBookworms"
+              nameKey="userIsActive"
+              cx="50%"
+              cy="50%"
+              outerRadius={60}
+              label
+            >
+              {dataForChart2.map((entry, index) => (
+                <Cell
+                  key={`cell-${entry.userIsActive}`}
+                  fill={colors[index % colors.length]}
+                />
+              ))}
+            </Pie>
+            <Pie
               data={dataForChart}
               dataKey="numberOfBookworms"
               nameKey="libraryName"
               cx="50%"
               cy="50%"
-              outerRadius={100}
+              outerRadius={90}
+              innerRadius={70}
               label
             >
               {dataForChart.map((entry, index) => (
