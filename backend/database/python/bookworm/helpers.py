0b396441e1c5f2db44c9426a0fe3f1124ef72ec0
Willem Verbuyst <willemverbuyst@gmail.com>
2023-04-13 08:10:32 +0200

    Get stats for bookworm

--- backend/database/python/bookworm/helpers.py
+++ backend/database/python/bookworm/helpers.py
@@ -13,3 +13,17 @@ def format_bookworms(bookworm_data):
         bookworms.append(bookworm)
 
     return bookworms
+
+
+def format_stats_libraries(library_data):
+    stats = []
+    for row in library_data:
+        data = {
+            "id": row[0],
+            "library_name": row[1],
+            "user_active": row[2],
+            "number_of_bookworms_per_library": row[3]
+        }
+        stats.append(data)
+
+    return stats
