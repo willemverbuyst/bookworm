76736cf24962003a2f78623be1fb454c746d7877
Willem Verbuyst <willemverbuyst@gmail.com>
2023-08-17 06:26:34 +0200

    Refactor library details modal

--- frontend/src/UI/components/Modal/BookwormModal.tsx
+++ frontend/src/UI/components/Modal/BookwormModal.tsx
@@ -8,27 +8,22 @@ import {
   ModalHeader,
   ModalOverlay,
 } from "@chakra-ui/react";
-import { useAppState } from "../../../business/overmind";
-import { LibraryDetailsCard } from "../../components/Cards/LibraryDetailsCard";
 
 interface Props {
   isOpen: boolean;
   onClose: () => void;
+  modalBody: React.ReactElement | null;
+  header: string;
 }
 
-export function LibraryDetails({ isOpen, onClose }: Props) {
-  const { details } = useAppState().library || {};
-
+export function BookwormModal({ isOpen, onClose, modalBody, header }: Props) {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Libraries</ModalHeader>
+        <ModalHeader>{header}</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
-          {details && <LibraryDetailsCard library={details} />}
-        </ModalBody>
-
+        <ModalBody>{modalBody}</ModalBody>
         <ModalFooter>
           <Button colorScheme="blue" mr={3} onClick={onClose}>
             Close
